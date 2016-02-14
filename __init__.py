8ff55757a00e8ad7ff996389b8f52213dcdf3ab2
tychovk <tychovk@gmail.com>
2016-02-13 18:01:34 -0600

    readjusted init

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
-from runserver import *
+from library import app
+
+from library.auth import *
+from library.app import *
+from library.search import *
+from library.helpers import *
+from library.db import *
+
+if __name__ == '__main__':
+	app.run()
